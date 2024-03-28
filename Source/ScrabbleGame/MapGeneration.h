0051adf54c90a9979335f117c2fd71ea0055e80b
Lucas Teo <jawsandeur@gmail.com>
2024-03-28 11:40:49 +0800

    Fix bugs, add translating function

--- Source/ScrabbleGame/MapGeneration.h
+++ Source/ScrabbleGame/MapGeneration.h
@@ -18,4 +18,8 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "MapGeneration")
 	static void GenerateCoordinates(const int32 Width, const int32 Height, const int32 NumPaths,
 		TArray<FVector2D> &Vertices, TArray<FVector2D> &Edges);
+
+	UFUNCTION(BlueprintCallable, Category = "MapGeneration")
+	static FVector2D TranslateToMapCoordinates(const FVector2D Point, const FVector2D Midpoint,
+		const int32 Width, const int32 Height);
 };
